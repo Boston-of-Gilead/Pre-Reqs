203d4698d1c9dcde2b9c235781b0aefc3c0c4fc6
Boston-of-Gilead <Boston-of-Gilead@users.noreply.github.com>
2021-06-25 10:24:51 -0400

    Add files via upload

---
+++ Pre-Reqs - RUN FIRST.ps1
@@ -0,0 +1,19 @@
+﻿Write-Host "----------------------------------"
+Write-Host "|   PRE-REQUISITES FOR SCRIPTS   |"
+Write-Host "----------------------------------"
+
+Write-Host "This script will install some modules for use in Powershell. You should only have to do this once, though as more scripts are developed, additional modules may be added."
+
+Install-Module MSOnline
+Install-Module AzureADPreview -Force
+install-module AzureAD
+Install-Module ExchangeOnlineManagement
+Install-Module ImportExcel
+Install-Module PSWindowsUpdate
+
+$ans = Read-Host -Prompt "Will you be using Powershell to administer VMWare? There is some overlap between the VMWare cmdlets and the standard cluster management cmdlets (Get-Cluster, New-Cluster, Remove-Cluster). Y/N"
+$ans = $ans.ToLower()
+If ($ans = 'y') {
+    Install-Module -Name VMware.PowerCLI -AllowClobber
+    }
+
